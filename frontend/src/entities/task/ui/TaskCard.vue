fcf14450e80e93996c2a381e310f62ddb294d3d6
evildes <renat.gogin1@gmail.com>
2025-03-23 03:41:58 +0300

    feat(task): add createdAt label and task completion checkbox
    - Added a label displaying the creation date of the task
    - Implemented a checkbox to mark the task as completed or not

--- frontend/src/entities/task/ui/TaskCard.vue
+++ frontend/src/entities/task/ui/TaskCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex justify-between items-start gap-4 text-left rounded-2xl p-6 bg-white shadow-sm w-full">
     <div class="flex flex-col flex-1 min-w-0">
+      <p class="text-sm">Дата создания: {{createdAtLabel}}</p>
       <input
           type="text"
           class="text-xl font-semibold mb-2 w-full"
@@ -15,29 +16,52 @@
           @input="onInputChange"
       />
     </div>
-    <div class="flex flex-col gap-2 font-medium shrink-0 text-right">
+    <div class="flex flex-col items-center gap-4 font-medium shrink-0 text-right">
       <p v-if="isModified" class="text-green-600 cursor-pointer" @click="saveChanges">Сохранить</p>
       <p class="text-red-500 cursor-pointer" @click="handleDelete">Удалить</p>
+      <input
+          type="checkbox"
+          :checked="isCompleted"
+          @change="toggleStatus"
+          class="form-checkbox h-7 w-7 text-green-600"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import {computed, ref, watch} from "vue";
+import { format } from 'date-fns';
 import type { Task } from "@/entities/task/types";
 import { useTaskStore } from "@/entities/task/store";
+import {TaskStatus} from "@/entities/task/enums.ts";
 
 const props = defineProps<{ task: Task }>();
+const taskStore = useTaskStore();
 
 const editedTask = ref({ ...props.task });
 const isModified = ref(false);
+
+const createdAtLabel = computed(() => {
+  return format(new Date(props.task.createdAt), "dd MMM yyyy, HH:mm");
+});
+const isCompleted = computed({
+  get: () => editedTask.value.status === TaskStatus.COMPLETED,
+  set: (val: boolean) => {
+    editedTask.value.status = val ? TaskStatus.COMPLETED : TaskStatus.NOT_COMPLETED;
+    isModified.value = true;
+  },
+});
 
 const onInputChange = () => {
   isModified.value = editedTask.value.name !== props.task.name ||
       editedTask.value.description !== props.task.description;
 };
 
-const taskStore = useTaskStore();
+const toggleStatus = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  isCompleted.value = target.checked;
+};
 
 const saveChanges = async () => {
   if (editedTask.value.id < 0) {
@@ -48,6 +72,7 @@ const saveChanges = async () => {
   await taskStore.updateTask(editedTask.value.id, {
     name: editedTask.value.name,
     description: editedTask.value.description,
+    status: editedTask.value.status,
   });
   isModified.value = false;
 };
