a65eece3c304f2e6618ecb21a3ace30e4dbf462f
evildes <renat.gogin1@gmail.com>
2025-03-24 16:01:08 +0300

    fix(user): handle authentication error messages

--- frontend/src/entities/user/ui/UserAuthForm.vue
+++ frontend/src/entities/user/ui/UserAuthForm.vue
@@ -1,4 +1,5 @@
 <template>
+  <p class="text-red-500">{{ errorMsg }}</p>
   <div class="flex flex-col gap-3">
     <pretty-input placeholder="username" type="text" v-model="authData.username" />
     <pretty-input placeholder="password" type="password" v-model="authData.password" />
@@ -11,7 +12,7 @@ import PrettyButton from "@/features/PrettyButton.vue";
 import PrettyInput from "@/features/PrettyInput.vue";
 import {useUserStore} from "@/entities/user/store.ts";
 import type {UserAuth} from "@/entities/user/types.ts";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import {goTo} from "@utils/navigation.ts";
 import {tasksListRoute} from "@/pages/task/routes.ts";
 
@@ -25,13 +26,18 @@ const authData = ref<UserAuth>({
   username: "",
   password: "",
 });
+const errorMsg = ref<string>("");
 
 const handleAuth = async () => {
   try {
     await userStore.auth(props.authType, authData.value);
     await goTo(tasksListRoute);
-  } catch (error) {
-    console.error("Error while authenticating:", error);
+  } catch (error: any) {
+    errorMsg.value = error?.response.data?.detail;
   }
 };
+
+watch(authData.value, () => {
+  errorMsg.value = "";
+})
 </script>
